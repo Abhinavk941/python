25c4b8a37ce0005f9ed876916b655a7aca80f86e
unknown <abhinavkarnatak941@gmail.com>
2022-03-23 00:34:14 +0530

    Initial Commit

---
+++ src/Duplicates.py
@@ -0,0 +1,11 @@
+l = []
+while(1):
+    a = int(input("Enter number : "))
+    try:
+        if a not in l and len(str(a)) == 2:
+            l.append(a)
+        else:
+            raise Exception
+    except:
+        print("Duplicates are not allowed and number should have 2 digits")
+        break
